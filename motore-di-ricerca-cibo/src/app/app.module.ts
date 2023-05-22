57174a572e5c89118c76a23315fbb827c53e87a0
CantoniRicardoFigc <cantoni.riccardo@iisgalvanimi.edu.it>
2023-05-22 07:55:48 +0000

    progetto finito bootstrap

--- motore-di-ricerca-cibo/src/app/app.module.ts
+++ motore-di-ricerca-cibo/src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ItemComponent } from './item/item.component';
 import { SearchComponent } from './search/search.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ItemComponent,
-    SearchComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SearchComponent, ItemComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
