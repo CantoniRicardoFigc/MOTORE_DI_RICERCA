3c0b10a9bac9c0f5ca078f31ba3ddd2af215a8fa
CantoniRicardoFigc <cantoni.riccardo@iisgalvanimi.edu.it>
2023-05-26 09:47:22 +0000

    versione aggiungi al carrello

--- motore-di-ricerca-cibo/src/app/item/item.component.ts
+++ motore-di-ricerca-cibo/src/app/item/item.component.ts
@@ -11,8 +11,10 @@ import { FoodService } from '../food.service';
 export class ItemComponent implements OnInit {
   routeObs: Observable<ParamMap> | undefined;
   foodServiceObs: Observable<Object> | undefined;
+  x = 0;
 
   items: any; //Qui salverò la traccia selezionata
+  isDisabled: boolean | undefined;
 
   constructor(
     private route: ActivatedRoute,
@@ -23,16 +25,44 @@ export class ItemComponent implements OnInit {
   ngOnInit(): void {
     this.routeObs = this.route.paramMap;
     this.routeObs.subscribe(this.getRouterParam);
+    
+    if (this.x >= 1) {
+      this.isDisabled = false;
+    } else {
+      this.isDisabled = true;
+    }
   }
 
   getRouterParam = (params: ParamMap) => {
     console.log(params);
     let itemId = params.get('id'); //Ottengo l'id dai parametri
     console.log(itemId); //Stampo su console
-    //spotifyServiceObs va dichiarato
     this.foodServiceObs = this.service.searchId(itemId);
     this.foodServiceObs.subscribe(
       (data) => ((this.items = data), console.log(data))
     );
   };
+
+  aggiungi = () => {
+    this.x += 1;
+
+    if (this.x >= 1) {
+      this.isDisabled = false;
+    } else {
+      this.isDisabled = true;
+    }
+  }
+
+  sottrai = () => {
+    if (this.x > 0) {
+      this.x -= 1
+    }
+
+    if (this.x >= 1) {
+      this.isDisabled = false;
+    } else {
+      this.isDisabled = true;
+    }
+  }
+
 }
